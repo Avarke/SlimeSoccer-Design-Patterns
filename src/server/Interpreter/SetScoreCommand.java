98013030cbedc5ab7570a495d98305db57ca3cc8
TitasJasevicius <titasjasevicius@gmail.com>
2025-12-14 17:59:38 +0200

    Iterator design pattern (in the world class)

---
+++ src/server/Interpreter/SetScoreCommand.java
@@ -0,0 +1,36 @@
+package server.Interpreter;
+
+import server.SlimeSoccer;
+
+/**
+ * /setscore <team1> <team2>
+ * Allows setting the scoreboard to arbitrary non-negative values.
+ */
+public class SetScoreCommand implements CommandExpression {
+    @Override
+    public String execute(String[] args) {
+        if (args == null || args.length < 2) {
+            return "Usage: /setscore <team1> <team2>";
+        }
+        try {
+            int t1 = Integer.parseInt(args[0]);
+            int t2 = Integer.parseInt(args[1]);
+            if (t1 < 0 || t2 < 0) {
+                return "Scores must be non-negative.";
+            }
+            // Update core game scores
+            SlimeSoccer.setScores(t1, t2);
+            // Keep other reporting commands in sync
+            ScoreCommand.updateScores(t1, t2);
+            ReportCommand.updateScores(t1, t2);
+            return "Scores updated: Team1=" + t1 + " Team2=" + t2;
+        } catch (NumberFormatException ex) {
+            return "Invalid numbers. Usage: /setscore <team1> <team2>";
+        }
+    }
+
+    @Override
+    public String getCommandName() {
+        return "setscore";
+    }
+}
